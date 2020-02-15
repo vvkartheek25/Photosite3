ef5899a0eedc03b430caba2505fba24b564207a9
Kartheek Vatti <vvkartheek967@gmail.com>
2020-02-15 11:44:55 -0800

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'user/index' => 'user#index'
   get 'photos/index' => 'photos#index'
+
     # get 'photos/index/:id' => 'photos#index'
 
     # resources :photos
